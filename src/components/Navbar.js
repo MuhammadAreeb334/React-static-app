3cbf0de18dd04e7d15989580edab80bc50893433
Areeb <muhammadareeb334@gmail.com>
2023-07-06 18:51:43 +0500

    commit changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,8 @@ import ReactLogo from "../Images/react-logo.png"
 function Navbar() {
     return(
         <nav className="navbar-Container">
-            <div className="navbar">
-                <img className="logo" src= {ReactLogo}  alt="logo of React"/>
-                <h1 className="heading-left">ReactFacts</h1>
-            </div>
+            <img className="logo" src= {ReactLogo}  alt="logo of React"/>
+            <h1 className="heading-left">ReactFacts</h1>    
             <h1 className="heading-right">React-course - project 2</h1>
         </nav>
     )
